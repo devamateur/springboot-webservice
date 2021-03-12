08eb79889af157e662f54a2e4e2341253ee12eb6
devamateur <ing06164@gmail.com>
2021-03-12 10:09:24 +0900

    응답 Dto를 관리하는 HelloResponseDto

---
+++ src/main/java/com/ing06164/springboot/web/Dto/HelloResponseDto.java
@@ -0,0 +1,11 @@
+package com.ing06164.springboot.web.Dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+    private final String name;
+    private final int amount;
+}
