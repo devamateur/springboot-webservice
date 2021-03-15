89ba44e76d6a06bb89a199fea41e61bdca0b9321
devamateur <ing06164@gmail.com>
2021-03-15 13:27:15 +0900

    Jpa Auditing을 활성화하는 어노테이션 추가

--- src/main/java/com/ing06164/springboot/Application.java
+++ src/main/java/com/ing06164/springboot/Application.java
@@ -2,7 +2,9 @@ package com.ing06164.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class Application {      // 메인 클래스, 항상 프로젝트의 최상단에 위치
     public static void main(String[] args){
