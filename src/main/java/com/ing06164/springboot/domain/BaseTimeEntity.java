f911d5df64a82deb1b6e2695538d248cf86cf47f
devamateur <ing06164@gmail.com>
2021-03-15 13:25:24 +0900

    Entity의 생성 시간과 수정 시간을 저장하는 클래스

---
+++ src/main/java/com/ing06164/springboot/domain/BaseTimeEntity.java
@@ -0,0 +1,22 @@
+package com.ing06164.springboot.domain;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseTimeEntity {
+
+    @CreatedDate
+    private LocalDateTime createdDate;      // Entity의 생성 시간 저장
+
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;     // Entity의 수정 시간 저장
+}
