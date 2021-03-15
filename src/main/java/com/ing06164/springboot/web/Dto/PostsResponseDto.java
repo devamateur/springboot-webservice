04cac8d10b3941b1ce42b41c556dbfc51b0ae8cd
devamateur <ing06164@gmail.com>
2021-03-15 12:12:59 +0900

    PostsResponseDto와 PostsUpdateRequestDto 추가

---
+++ src/main/java/com/ing06164/springboot/web/Dto/PostsResponseDto.java
@@ -0,0 +1,18 @@
+package com.ing06164.springboot.web.Dto;
+
+import com.ing06164.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity){
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.author = entity.getAuthor();
+    }
+}
