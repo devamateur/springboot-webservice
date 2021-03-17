db4d84217b784a043cf5dda918652129a1f67375
devamateur <ing06164@gmail.com>
2021-03-17 11:07:43 +0900

    IndexController: index.mustache에 url 매핑

---
+++ src/main/java/com/ing06164/springboot/web/IndexController.java
@@ -0,0 +1,13 @@
+package com.ing06164.springboot.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+    
+    @GetMapping("/")
+    public String index(){      // 컨트롤러에서 문자열을 반환할 때, 머스테치 스타터가 파일 경로와 확장자를 자동으로 지정함
+        return "index";
+    }
+}
