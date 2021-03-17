55a4e1f2fcc43ba75c205a6211fd7332b73dbe7a
devamateur <ing06164@gmail.com>
2021-03-17 11:42:53 +0900

    posts/save 주소에 해당하는 컨트롤러 추가

--- src/main/java/com/ing06164/springboot/web/IndexController.java
+++ src/main/java/com/ing06164/springboot/web/IndexController.java
@@ -10,4 +10,9 @@ public class IndexController {
     public String index(){      // 컨트롤러에서 문자열을 반환할 때, 머스테치 스타터가 파일 경로와 확장자를 자동으로 지정함
         return "index";
     }
+
+    @GetMapping("/posts/save")
+    public String postsSave(){
+        return "posts-save";
+    }
 }
