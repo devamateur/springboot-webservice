3b82309bf07716f71ddff692cee817692453d250
devamateur <ing06164@gmail.com>
2021-03-18 16:02:19 +0900

    index() 메소드에서 Model 객체를 인자로 넘겨 줌

--- src/main/java/com/ing06164/springboot/web/IndexController.java
+++ src/main/java/com/ing06164/springboot/web/IndexController.java
@@ -1,13 +1,20 @@
 package com.ing06164.springboot.web;
 
+import com.ing06164.springboot.service.posts.PostsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
-    
+
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String index(){      // 컨트롤러에서 문자열을 반환할 때, 머스테치 스타터가 파일 경로와 확장자를 자동으로 지정함
+    public String index(Model model){      // 컨트롤러에서 문자열을 반환할 때, 머스테치 스타터가 파일 경로와 확장자를 자동으로 지정함
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
     }
 
