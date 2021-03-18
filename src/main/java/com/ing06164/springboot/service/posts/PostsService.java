7734381f134acd1af706fd5839b9c7d69288858c
devamateur <ing06164@gmail.com>
2021-03-18 15:59:37 +0900

    findAllDesc(): Posts를 PostsListResponseDto로 변환, List로 반환하는 메소드

--- src/main/java/com/ing06164/springboot/service/posts/PostsService.java
+++ src/main/java/com/ing06164/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.ing06164.springboot.service.posts;
 
 import com.ing06164.springboot.domain.posts.Posts;
 import com.ing06164.springboot.domain.posts.PostsRepository;
+import com.ing06164.springboot.web.Dto.PostsListResponseDto;
 import com.ing06164.springboot.web.Dto.PostsResponseDto;
 import com.ing06164.springboot.web.Dto.PostsSaveRequestDto;
 import com.ing06164.springboot.web.Dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -32,5 +36,12 @@ public class PostsService {
         Posts entity = postsRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)     // 트랜잭션의 조회 기능만 남겨둠(등록/수정/삭제X), 조회 속도 개선
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
