f23d079b83bb8f77ee06c28a722fea8371932d04
devamateur <ing06164@gmail.com>
2021-04-01 10:42:25 +0900

    delete() 추가

--- src/main/java/com/ing06164/springboot/web/PostsApiController.java
+++ src/main/java/com/ing06164/springboot/web/PostsApiController.java
@@ -1,11 +1,14 @@
 package com.ing06164.springboot.web;
 
 import com.ing06164.springboot.service.posts.PostsService;
+import com.ing06164.springboot.web.Dto.PostsListResponseDto;
 import com.ing06164.springboot.web.Dto.PostsResponseDto;
 import com.ing06164.springboot.web.Dto.PostsSaveRequestDto;
 import com.ing06164.springboot.web.Dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -14,17 +17,28 @@ public class PostsApiController {
     private final PostsService postsService;
 
     @PostMapping("/api/v1/posts")
-    public Long save(@RequestBody PostsSaveRequestDto requestDto){
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
         return postsService.save(requestDto);
     }
 
     @PutMapping("/api/v1/posts/{id}")
-    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
         return postsService.update(id, requestDto);
     }
 
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id) {
+        postsService.delete(id);
+        return id;
+    }
+
     @GetMapping("/api/v1/posts/{id}")
-    public PostsResponseDto findById(@PathVariable Long id){
+    public PostsResponseDto findById(@PathVariable Long id) {
         return postsService.findById(id);
+    }
+
+    @GetMapping("/api/v1/posts/list")
+    public List<PostsListResponseDto> findAll() {
+        return postsService.findAllDesc();
     }
 }
