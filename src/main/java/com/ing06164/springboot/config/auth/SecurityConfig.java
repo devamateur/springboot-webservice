652ce3a3a838687544792625728336e01029e3f7
devamateur <ing06164@gmail.com>
2021-04-04 12:51:18 +0900

    SecurityConfig

--- src/main/java/com/ing06164/springboot/config/auth/SecurityConfig.java
+++ src/main/java/com/ing06164/springboot/config/auth/SecurityConfig.java
@@ -1,4 +1,34 @@
 package com.ing06164.springboot.config.auth;
 
-public class SecurityConfig {
+import com.ing06164.springboot.domain.user.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                    .authorizeRequests()    // URL 별 권한 관리 설정 시작점
+                    .antMatchers("/", "/css/**", "/images/**", // 지정된 URL에 한해 전체 열람 권한 부여
+                            "/js/**", "/h2-console/**", "/profile").permitAll()
+                    .antMatchers("/api/v1/**").hasRole(Role.USER.name())      // 이 URL은 user권한을 가진 사람만
+                    .anyRequest().authenticated()
+                .and()
+                    .logout()
+                        .logoutSuccessUrl("/")
+                .and()
+                    .oauth2Login()
+                        .userInfoEndpoint()
+                        .userService(customOAuth2UserService);
+    }
 }
