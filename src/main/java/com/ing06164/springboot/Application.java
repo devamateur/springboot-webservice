77f535dc97b130ddf9bef1e7fbe56c7d81bc7057
devamateur <ing06164@gmail.com>
2021-04-06 16:45:59 +0900

    @EnableJpaAuditing 제거

--- src/main/java/com/ing06164/springboot/Application.java
+++ src/main/java/com/ing06164/springboot/Application.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 @SpringBootApplication
 public class Application {      // 메인 클래스, 항상 프로젝트의 최상단에 위치
     public static void main(String[] args){
