c247f2b12b812bc4e468cd14ea8a13c022eb4290
devamateur <ing06164@gmail.com>
2021-04-06 15:09:16 +0900

    IndexController의 index() 메소드 수정 -

--- src/main/java/com/ing06164/springboot/web/IndexController.java
+++ src/main/java/com/ing06164/springboot/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.ing06164.springboot.web;
 
+import com.ing06164.springboot.config.auth.LoginUser;
+import com.ing06164.springboot.config.auth.dto.SessionUser;
 import com.ing06164.springboot.service.posts.PostsService;
 import com.ing06164.springboot.web.Dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,6 +10,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
@@ -15,8 +19,11 @@ public class IndexController {
     private final PostsService postsService;
 
     @GetMapping("/")
-    public String index(Model model){      // 컨트롤러에서 문자열을 반환할 때, 머스테치 스타터가 파일 경로와 확장자를 자동으로 지정함
+    public String index(Model model, @LoginUser SessionUser user){      // 컨트롤러에서 문자열을 반환할 때, 머스테치 스타터가 파일 경로와 확장자를 자동으로 지정함
         model.addAttribute("posts", postsService.findAllDesc());
+
+        if(user != null)
+            model.addAttribute("userName", user.getName());
         return "index";
     }
 
