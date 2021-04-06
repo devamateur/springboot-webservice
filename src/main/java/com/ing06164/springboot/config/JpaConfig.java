a7b2df7a14a8454f77fb65b42ce8c4ba9f8b2eee
devamateur <ing06164@gmail.com>
2021-04-06 16:48:25 +0900

    application에서 @EnableJpaAuditing을 제거한 대신 여기에 추가

---
+++ src/main/java/com/ing06164/springboot/config/JpaConfig.java
@@ -0,0 +1,8 @@
+package com.ing06164.springboot.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig { }
